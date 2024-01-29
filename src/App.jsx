3bf57b0788cdb6fe61fe2f1021bf2109ee06e46f
Yuta Home <mizunoyuta1014@gmail.com>
2024-01-29 15:43:20 +0900

    初期設定

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
